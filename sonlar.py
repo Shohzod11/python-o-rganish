d0cda301a0dba52347139a7e99840f7fc5d53d58
Raymboyev Shohzod <89315068+Shohzod11@users.noreply.github.com>
2021-10-04 21:02:40 +0500

    Add files via upload

---
+++ sonlar.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 19 17:15:58 2021
+
+@author: Professional
+"""
+
+#son = int(input("Biror son kiriting: ") )
+#print(son, "ning kvadrati", son*son, "ga teng")
+#print (son, "ning kubi", son*son*son, "ga teng")
+
+#yosh = int(input("Yoshingiz nechida: "))
+#print("Siz", 2021 - yosh, "yilda tug'ilgansz")
+
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+print("yig'indisi ", a + b)
+print("ayirmasi ", a - b)
+print("bo'linmasi ", a/b)
+print("ko'paytmasi ", a*b)
