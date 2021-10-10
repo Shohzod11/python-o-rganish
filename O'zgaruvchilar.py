591befb9b10baa4ddd76691dbfe8fdfa027a108c
Raymboyev Shohzod <89315068+Shohzod11@users.noreply.github.com>
2021-10-10 16:23:28 +0500

    Add files via upload

---
+++ O'zgaruvchilar.py
@@ -0,0 +1,13 @@
+kocha = "Bog'bon "
+mahalla = "Sog'bon "
+tuman = "Bodomzor "
+viloyat = "Samarqand "
+print(kocha + "ko'chasi " + mahalla + "mahallasi " + tuman + "tumani " + viloyat + "viloyati" )
+
+print("Iltimos, quyidagi ma'lumotlarni kiriting:")
+kocha = input("Ko'changiz: ")
+mahalla = input("Mahallangiz: ")
+tuman = input("Tumaningiz: ")
+viloyat = input("Viloyatingiz: ")
+print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+     tuman + " tumani, " + viloyat + " viloyati")  
